a7ba5652d462f03ccc186d22226a539433f4e0bb
prahasi kacham <prahasi kacham>
2016-06-10 18:26:04 -0400

    error line chart with raw data, prediction line and error areas

--- src/XPlot.Plotly/Tests/ErrorLineChart.fsx
+++ src/XPlot.Plotly/Tests/ErrorLineChart.fsx
@@ -1,4 +1,4 @@
-﻿#load "../packages/FSLab.0.3.19/FsLab.fsx"
+﻿#load "../packages/FSLab.0.3.20/FsLab.fsx"
 
 open System
 open XPlot.Plotly
@@ -7,25 +7,36 @@ type Chartable = {XD:DateTime; YD:float}
 
 // Raw Data
 let tD1 = [|
-                        { XD = System.DateTime.Parse("5/13/2016"); YD = 4.0 };
+                        { XD = System.DateTime.Parse("5/13/2016"); YD = 2.0 };
                         { XD = System.DateTime.Parse("5/12/2016"); YD = 6.0 };
-                        { XD = System.DateTime.Parse("5/9/2016");  YD = 1.5 };
+                        { XD = System.DateTime.Parse("5/9/2016");  YD = 1.0 };
                         { XD = System.DateTime.Parse("5/20/2016"); YD = 8.0 }
                             |]
 // Prediction Line
 let tD2 = [|
-                        { XD = System.DateTime.Parse("5/9/2016");  YD = 3.0 };
-                        { XD = System.DateTime.Parse("5/20/2016"); YD = 7.0 }
+                        { XD = System.DateTime.Parse("5/9/2016");  YD = 0.0 };
+                        { XD = System.DateTime.Parse("5/12/2016");  YD = 2.0 };
+                        { XD = System.DateTime.Parse("5/13/2016");  YD = 6.0 };
+                        { XD = System.DateTime.Parse("5/20/2016"); YD = 7.0 };
+                        { XD = System.DateTime.Parse("5/22/2016"); YD = 7.0 }
                             |]
 // Upper Bound
 let tD3 = [|
-                        { XD = System.DateTime.Parse("5/9/2016");  YD = 4.0 };
-                        { XD = System.DateTime.Parse("5/20/2016"); YD = 7.0 }
+                        { XD = System.DateTime.Parse("5/9/2016");  YD = 1.6 };
+                        { XD = System.DateTime.Parse("5/12/2016");  YD = 3.0 };
+                        { XD = System.DateTime.Parse("5/13/2016"); YD = 7.0 };
+                        { XD = System.DateTime.Parse("5/20/2016"); YD = 7.9 };
+                        { XD = System.DateTime.Parse("5/22/2016"); YD = 7.9 }
                             |]
 // Lower Bound
 let tD4 = [|
-                        { XD = System.DateTime.Parse("5/9/2016");  YD = 2.0 };
-                        { XD = System.DateTime.Parse("5/20/2016"); YD = 7.0 }
+                        { XD = System.DateTime.Parse("5/9/2016");  YD = -1.5 };
+                        { XD = System.DateTime.Parse("5/12/2016");  YD = 0.9 };
+                        { XD = System.DateTime.Parse("5/13/2016");  YD = 5.0 };
+                        { XD = System.DateTime.Parse("5/20/2016");  YD = 6.1 };
+                        // { XD = System.DateTime.Parse("5/20/2016"); YD = 7.0 };
+                        { XD = System.DateTime.Parse("5/22/2016"); YD = 6.2 }
+                        
                             |]
 
 let getXVal (data:Chartable array) =
@@ -83,8 +94,11 @@ let rawData =
             visible = true;
         },
         *)
+        
         mode = "lines+markers",
+        text = ["y=mx+b"],
         name = "Raw Data"
+        
     )
 let predictionLine =
     Scatter(
@@ -98,7 +112,7 @@ let upperError =
         x = upErrX,
         y = upErrY,
         fill = "tonexty", 
-        fillcolor = "rgba(255, 153, 0, 0.3)", 
+        fillcolor = "rgba(255, 255, 153, 0.2)", 
         mode = "text",
         name = "Upper Bound"
     )
@@ -107,10 +121,11 @@ let lowerError =
         x = loErrX,
         y = loErrY,
         fill = "tonexty", 
-        fillcolor = "rgba(255, 153, 0, 0.3)",
+        fillcolor = "rgba(255, 255, 153, 0.2)",
         mode = "text", 
         name = "Lower Bound"
     )
 
 [rawData; predictionLine; upperError; lowerError;]
 |> Plotly.Plot
+
